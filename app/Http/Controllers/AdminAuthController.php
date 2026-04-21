fc8a314e25dc5347f89caa71dde3a35d8ba602f1
orlandoroca <rocahuapaya@hotmail.com>
2026-04-21 12:39:39 -0500

    update

--- app/Http/Controllers/AdminAuthController.php
+++ app/Http/Controllers/AdminAuthController.php
@@ -4,10 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cookie;
 use Illuminate\View\View;
 
 class AdminAuthController extends Controller
 {
+    private const ADMIN_AUTH_COOKIE = 'remates_admin_auth';
+
     public function showLogin(): View
     {
         return view('admin.remates.login');
@@ -25,6 +28,17 @@ class AdminAuthController extends Controller
 
         $request->session()->put('remates_admin_auth', true);
         $request->session()->regenerate();
+        Cookie::queue(cookie(
+            self::ADMIN_AUTH_COOKIE,
+            '1',
+            60 * 24 * 30,
+            '/',
+            config('session.domain'),
+            config('session.secure'),
+            true,
+            false,
+            config('session.same_site', 'lax')
+        ));
 
         return redirect()->route('admin.remates.index');
     }
@@ -34,6 +48,7 @@ class AdminAuthController extends Controller
         $request->session()->forget('remates_admin_auth');
         $request->session()->invalidate();
         $request->session()->regenerateToken();
+        Cookie::queue(Cookie::forget(self::ADMIN_AUTH_COOKIE, '/', config('session.domain')));
 
         return redirect()->route('admin.remates.login');
     }
--- app/Http/Middleware/EnsureRematesAdminAuthenticated.php
+++ app/Http/Middleware/EnsureRematesAdminAuthenticated.php
@@ -8,9 +8,17 @@ use Symfony\Component\HttpFoundation\Response;
 
 class EnsureRematesAdminAuthenticated
 {
+    private const ADMIN_AUTH_COOKIE = 'remates_admin_auth';
+
     public function handle(Request $request, Closure $next): Response
     {
         if ($request->session()->get('remates_admin_auth') === true) {
+            return $next($request);
+        }
+
+        if ($request->cookie(self::ADMIN_AUTH_COOKIE) === '1') {
+            $request->session()->put('remates_admin_auth', true);
+
             return $next($request);
         }
 
